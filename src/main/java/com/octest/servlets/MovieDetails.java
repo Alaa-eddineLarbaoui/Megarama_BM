a58096e789d9aa0e940bc6843b20c9b81713cf4b
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-04-25 16:44:19 +0100

    j'ai ajouter html de la page detail movie et autre chose

---
+++ src/main/java/com/octest/servlets/MovieDetails.java
@@ -0,0 +1,20 @@
+package com.octest.servlets;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "MovieDetails", value = "/detail")
+public class MovieDetails extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/DetailMovie.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
