51101609dc3886196d288147663503a8e3caccba
cherkaoui oussama <oussamacherkaoui2019@gmail.com>
2024-04-28 15:34:04 +0100

    merge main et authentification

--- src/main/java/dao/FilmDAOImpl.java
+++ src/main/java/dao/FilmDAOImpl.java
@@ -13,21 +13,17 @@ import java.util.List;
 public class FilmDAOImpl implements FilmDAO{
     @Override
     public List<Films> ShowFilms() throws SQLException, ClassNotFoundException {
-        ArrayList<Films> films=new ArrayList<>();
-        String sql="SELECT film_id, titre , picture FROM films";
-        PreparedStatement statement = ConnectionDAO.getConnection().prepareStatement(sql);
-        ResultSet resultat = statement.executeQuery();
+            ArrayList<Films> films=new ArrayList<>();
+            String sql="SELECT film_id, titre , picture FROM films";
+            PreparedStatement statement = ConnectionDAO.getConnection().prepareStatement(sql);
+            ResultSet resultat = statement.executeQuery();
 
-        while (resultat.next()) {
-            Integer id_film = resultat.getInt("film_id");
-             String title=resultat.getString("titre");
-           String director =resultat.getString("director");
-            String genre = resultat.getString("genre");
-            Integer duration=resultat.getInt("duration");
-            String synopsis=resultat.getString("synopsis");
-            Films flm=new Films(id_film,title,director,genre,duration,synopsis);
-            films.add(flm);
-
+            while (resultat.next()) {
+                Integer id_film = resultat.getInt("film_id");
+                String title=resultat.getString("titre");
+                String picture=resultat.getString("picture");
+                Films flm=new Films(id_film,title,picture);
+                films.add(flm);
 
 
 
